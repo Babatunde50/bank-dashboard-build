f9077eae369e266899ab56618540a42d19c18c5b
babatunde50 <babatundeololade50@gmail.com>
2020-10-05 11:16:23 +0100

    initial commit

---
+++ precache-manifest.75ebe20b5413c74124fd7c50b4bdeb6e.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d051cd07fb3ef57a672e53fda83b0b7f",
+    "url": "/bank-dashboard-ui/index.html"
+  },
+  {
+    "revision": "6292d062a19a0be03538",
+    "url": "/bank-dashboard-ui/static/css/main.48c9435a.chunk.css"
+  },
+  {
+    "revision": "8228a06d5b2004e98773",
+    "url": "/bank-dashboard-ui/static/js/2.ef561f5c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/bank-dashboard-ui/static/js/2.ef561f5c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6292d062a19a0be03538",
+    "url": "/bank-dashboard-ui/static/js/main.ad605519.chunk.js"
+  },
+  {
+    "revision": "fa95a16d3620b6ea2afd",
+    "url": "/bank-dashboard-ui/static/js/runtime-main.01bca5a9.js"
+  },
+  {
+    "revision": "52fc25da25d5a13020b1a6532f7decf6",
+    "url": "/bank-dashboard-ui/static/media/Map.52fc25da.png"
+  },
+  {
+    "revision": "c95621268fe981413e252bbd74f4f30e",
+    "url": "/bank-dashboard-ui/static/media/account_balance-24px.c9562126.svg"
+  },
+  {
+    "revision": "e214fea606b50c9fe90e7608c15ab880",
+    "url": "/bank-dashboard-ui/static/media/visa_card.e214fea6.png"
+  },
+  {
+    "revision": "e5e40c5833c6a54ba99c87f49d34e396",
+    "url": "/bank-dashboard-ui/static/media/visa_card2.e5e40c58.png"
+  }
+]);
